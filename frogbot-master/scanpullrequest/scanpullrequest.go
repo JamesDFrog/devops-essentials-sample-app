b7bee18f6f59fe17f45e43b3049a9c96b2987cce
JamesDFrog <jamesd@jfrog.com>
2024-10-21 13:22:28 -0700

    adding frogbot

---
+++ frogbot-master/scanpullrequest/scanpullrequest.go
@@ -0,0 +1,363 @@
+package scanpullrequest
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/jfrog/frogbot/v2/utils"
+	"github.com/jfrog/froggit-go/vcsclient"
+	"github.com/jfrog/froggit-go/vcsutils"
+	"github.com/jfrog/gofrog/datastructures"
+	"github.com/jfrog/jfrog-cli-security/utils/formats"
+	"github.com/jfrog/jfrog-cli-security/utils/jasutils"
+	"github.com/jfrog/jfrog-cli-security/utils/results"
+	"github.com/jfrog/jfrog-cli-security/utils/results/conversion"
+	"github.com/jfrog/jfrog-cli-security/utils/xsc"
+	"github.com/jfrog/jfrog-client-go/utils/log"
+	"github.com/jfrog/jfrog-client-go/xray/services"
+)
+
+const (
+	SecurityIssueFoundErr   = "issues were detected by Frogbot\n You can avoid marking the Frogbot scan as failed by setting failOnSecurityIssues to false in the " + utils.FrogbotConfigFile + " file"
+	noGitHubEnvErr          = "frogbot did not scan this PR, because a GitHub Environment named 'frogbot' does not exist. Please refer to the Frogbot documentation for instructions on how to create the Environment"
+	noGitHubEnvReviewersErr = "frogbot did not scan this PR, because the existing GitHub Environment named 'frogbot' doesn't have reviewers selected. Please refer to the Frogbot documentation for instructions on how to create the Environment"
+	analyticsScanPrScanType = "PR"
+)
+
+type ScanPullRequestCmd struct{}
+
+// Run ScanPullRequest method only works for a single repository scan.
+// Therefore, the first repository config represents the repository on which Frogbot runs, and it is the only one that matters.
+func (cmd *ScanPullRequestCmd) Run(configAggregator utils.RepoAggregator, client vcsclient.VcsClient, frogbotRepoConnection *utils.UrlAccessChecker) (err error) {
+	if err = utils.ValidateSingleRepoConfiguration(&configAggregator); err != nil {
+		return
+	}
+	repoConfig := &(configAggregator)[0]
+	if repoConfig.GitProvider == vcsutils.GitHub {
+		if err = verifyGitHubFrogbotEnvironment(client, repoConfig); err != nil {
+			return
+		}
+	}
+	repoConfig.OutputWriter.SetHasInternetConnection(frogbotRepoConnection.IsConnected())
+	if repoConfig.PullRequestDetails, err = client.GetPullRequestByID(context.Background(), repoConfig.RepoOwner, repoConfig.RepoName, int(repoConfig.PullRequestDetails.ID)); err != nil {
+		return
+	}
+	return scanPullRequest(repoConfig, client)
+}
+
+// Verify that the 'frogbot' GitHub environment was properly configured on the repository
+func verifyGitHubFrogbotEnvironment(client vcsclient.VcsClient, repoConfig *utils.Repository) error {
+	if repoConfig.APIEndpoint != "" && repoConfig.APIEndpoint != "https://api.github.com" {
+		// Don't verify 'frogbot' environment on GitHub on-prem
+		return nil
+	}
+	if _, exist := os.LookupEnv(utils.GitHubActionsEnv); !exist {
+		// Don't verify 'frogbot' environment on non GitHub Actions CI
+		return nil
+	}
+
+	// If the repository is not public, using 'frogbot' environment is not mandatory
+	repoInfo, err := client.GetRepositoryInfo(context.Background(), repoConfig.RepoOwner, repoConfig.RepoName)
+	if err != nil {
+		return err
+	}
+	if repoInfo.RepositoryVisibility != vcsclient.Public {
+		return nil
+	}
+
+	// Get the 'frogbot' environment info and make sure it exists and includes reviewers
+	repoEnvInfo, err := client.GetRepositoryEnvironmentInfo(context.Background(), repoConfig.RepoOwner, repoConfig.RepoName, "frogbot")
+	if err != nil {
+		return errors.New(err.Error() + "\n" + noGitHubEnvErr)
+	}
+	if len(repoEnvInfo.Reviewers) == 0 {
+		return errors.New(noGitHubEnvReviewersErr)
+	}
+
+	return nil
+}
+
+// By default, includeAllVulnerabilities is set to false and the scan goes as follows:
+// a. Audit the dependencies of the source and the target branches.
+// b. Compare the vulnerabilities found in source and target branches, and show only the new vulnerabilities added by the pull request.
+// Otherwise, only the source branch is scanned and all found vulnerabilities are being displayed.
+func scanPullRequest(repo *utils.Repository, client vcsclient.VcsClient) (err error) {
+	pullRequestDetails := repo.PullRequestDetails
+	log.Info(fmt.Sprintf("Scanning Pull Request #%d (from source branch: <%s/%s/%s> to target branch: <%s/%s/%s>)",
+		pullRequestDetails.ID,
+		pullRequestDetails.Source.Owner, pullRequestDetails.Source.Repository, pullRequestDetails.Source.Name,
+		pullRequestDetails.Target.Owner, pullRequestDetails.Target.Repository, pullRequestDetails.Target.Name))
+	log.Info("-----------------------------------------------------------")
+
+	analyticsService := utils.AddAnalyticsGeneralEvent(nil, &repo.Server, analyticsScanPrScanType)
+	defer func() {
+		analyticsService.UpdateAndSendXscAnalyticsGeneralEventFinalize(err)
+	}()
+
+	// Audit PR code
+	issues, err := auditPullRequest(repo, client, analyticsService)
+	if err != nil {
+		return
+	}
+
+	// Output results
+	shouldSendExposedSecretsEmail := issues.SecretsExists() && repo.SmtpServer != ""
+	if shouldSendExposedSecretsEmail {
+		secretsEmailDetails := utils.NewSecretsEmailDetails(client, repo, issues.Secrets)
+		if err = utils.AlertSecretsExposed(secretsEmailDetails); err != nil {
+			return
+		}
+	}
+
+	// Handle PR comments for scan output
+	if err = utils.HandlePullRequestCommentsAfterScan(issues, repo, client, int(pullRequestDetails.ID)); err != nil {
+		return
+	}
+
+	// Fail the Frogbot task if a security issue is found and Frogbot isn't configured to avoid the failure.
+	if toFailTaskStatus(repo, issues) {
+		err = errors.New(SecurityIssueFoundErr)
+		return
+	}
+	return
+}
+
+func toFailTaskStatus(repo *utils.Repository, issues *utils.IssuesCollection) bool {
+	failFlagSet := repo.FailOnSecurityIssues != nil && *repo.FailOnSecurityIssues
+	return failFlagSet && issues.IssuesExists()
+}
+
+// Downloads Pull Requests branches code and audits them
+func auditPullRequest(repoConfig *utils.Repository, client vcsclient.VcsClient, analyticsService *xsc.AnalyticsMetricsService) (issuesCollection *utils.IssuesCollection, err error) {
+	scanDetails := utils.NewScanDetails(client, &repoConfig.Server, &repoConfig.Git).
+		SetXrayGraphScanParams(repoConfig.Watches, repoConfig.JFrogProjectKey, len(repoConfig.AllowedLicenses) > 0).
+		SetFixableOnly(repoConfig.FixableOnly).
+		SetFailOnInstallationErrors(*repoConfig.FailOnSecurityIssues).
+		SetConfigProfile(repoConfig.ConfigProfile).
+		SetSkipAutoInstall(repoConfig.SkipAutoInstall)
+	if scanDetails, err = scanDetails.SetMinSeverity(repoConfig.MinSeverity); err != nil {
+		return
+	}
+
+	// If MSI exists we always need to report events
+	if analyticsService.GetMsi() != "" {
+		// MSI is passed to XrayGraphScanParams, so it can be later used by other analytics events in the scan phase
+		scanDetails.XrayGraphScanParams.MultiScanId = analyticsService.GetMsi()
+	}
+
+	issuesCollection = &utils.IssuesCollection{}
+	for i := range repoConfig.Projects {
+		scanDetails.SetProject(&repoConfig.Projects[i])
+		var projectIssues *utils.IssuesCollection
+		if projectIssues, err = auditPullRequestInProject(repoConfig, scanDetails); err != nil {
+			return
+		}
+		issuesCollection.Append(projectIssues)
+	}
+	if analyticsService.ShouldReportEvents() {
+		analyticsService.AddScanFindingsToXscAnalyticsGeneralEventFinalize(issuesCollection.CountIssuesCollectionFindings())
+	}
+	return
+}
+
+func auditPullRequestInProject(repoConfig *utils.Repository, scanDetails *utils.ScanDetails) (auditIssues *utils.IssuesCollection, err error) {
+	// Download source branch
+	sourcePullRequestInfo := scanDetails.PullRequestDetails.Source
+	sourceBranchWd, cleanupSource, err := utils.DownloadRepoToTempDir(scanDetails.Client(), sourcePullRequestInfo.Owner, sourcePullRequestInfo.Repository, sourcePullRequestInfo.Name)
+	if err != nil {
+		return
+	}
+	defer func() {
+		err = errors.Join(err, cleanupSource())
+	}()
+
+	// Audit source branch
+	var sourceResults *results.SecurityCommandResults
+	workingDirs := utils.GetFullPathWorkingDirs(scanDetails.Project.WorkingDirs, sourceBranchWd)
+	log.Info("Scanning source branch...")
+	sourceResults, err = scanDetails.RunInstallAndAudit(workingDirs...)
+	if err != nil {
+		return
+	}
+
+	// Set JAS output flags
+	repoConfig.OutputWriter.SetJasOutputFlags(sourceResults.EntitledForJas, len(sourceResults.GetJasScansResults(jasutils.Applicability)) > 0)
+
+	// Get all issues that exist in the source branch
+	if repoConfig.IncludeAllVulnerabilities {
+		if auditIssues, err = getAllIssues(sourceResults, repoConfig.AllowedLicenses, scanDetails.HasViolationContext()); err != nil {
+			return
+		}
+		utils.ConvertSarifPathsToRelative(auditIssues, sourceBranchWd)
+		return
+	}
+
+	var targetBranchWd string
+	if auditIssues, targetBranchWd, err = auditTargetBranch(repoConfig, scanDetails, sourceResults); err != nil {
+		return
+	}
+	utils.ConvertSarifPathsToRelative(auditIssues, sourceBranchWd, targetBranchWd)
+	return
+}
+
+func auditTargetBranch(repoConfig *utils.Repository, scanDetails *utils.ScanDetails, sourceScanResults *results.SecurityCommandResults) (newIssues *utils.IssuesCollection, targetBranchWd string, err error) {
+	// Download target branch (if needed)
+	cleanupTarget := func() error { return nil }
+	if !repoConfig.IncludeAllVulnerabilities {
+		targetBranchInfo := repoConfig.PullRequestDetails.Target
+		if targetBranchWd, cleanupTarget, err = utils.DownloadRepoToTempDir(scanDetails.Client(), targetBranchInfo.Owner, targetBranchInfo.Repository, targetBranchInfo.Name); err != nil {
+			return
+		}
+	}
+	defer func() {
+		err = errors.Join(err, cleanupTarget())
+	}()
+
+	// Set target branch scan details
+	var targetResults *results.SecurityCommandResults
+	workingDirs := utils.GetFullPathWorkingDirs(scanDetails.Project.WorkingDirs, targetBranchWd)
+	log.Info("Scanning target branch...")
+	targetResults, err = scanDetails.RunInstallAndAudit(workingDirs...)
+	if err != nil {
+		return
+	}
+
+	// Get newly added issues
+	newIssues, err = getNewlyAddedIssues(targetResults, sourceScanResults, repoConfig.AllowedLicenses, scanDetails.HasViolationContext())
+	return
+}
+
+func getAllIssues(cmdResults *results.SecurityCommandResults, allowedLicenses []string, hasViolationContext bool) (*utils.IssuesCollection, error) {
+	log.Info("Frogbot is configured to show all vulnerabilities")
+	simpleJsonResults, err := conversion.NewCommandResultsConvertor(conversion.ResultConvertParams{
+		IncludeVulnerabilities: true,
+		HasViolationContext:    hasViolationContext,
+		AllowedLicenses:        allowedLicenses,
+		IncludeLicenses:        true,
+		SimplifiedOutput:       true,
+	}).ConvertToSimpleJson(cmdResults)
+	if err != nil {
+		return nil, err
+	}
+	return &utils.IssuesCollection{
+		Vulnerabilities: append(simpleJsonResults.Vulnerabilities, simpleJsonResults.SecurityViolations...),
+		Iacs:            simpleJsonResults.Iacs,
+		Secrets:         simpleJsonResults.Secrets,
+		Sast:            simpleJsonResults.Sast,
+		Licenses:        simpleJsonResults.LicensesViolations,
+	}, nil
+}
+
+// Returns all the issues found in the source branch that didn't exist in the target branch.
+func getNewlyAddedIssues(targetResults, sourceResults *results.SecurityCommandResults, allowedLicenses []string, hasViolationContext bool) (*utils.IssuesCollection, error) {
+	var err error
+	convertor := conversion.NewCommandResultsConvertor(conversion.ResultConvertParams{IncludeVulnerabilities: true, HasViolationContext: hasViolationContext, IncludeLicenses: len(allowedLicenses) > 0, AllowedLicenses: allowedLicenses, SimplifiedOutput: true})
+	simpleJsonSource, err := convertor.ConvertToSimpleJson(sourceResults)
+	if err != nil {
+		return nil, err
+	}
+	simpleJsonTarget, err := convertor.ConvertToSimpleJson(targetResults)
+	if err != nil {
+		return nil, err
+	}
+
+	var newVulnerabilitiesOrViolations []formats.VulnerabilityOrViolationRow
+	if len(simpleJsonSource.Vulnerabilities) > 0 || len(simpleJsonSource.SecurityViolations) > 0 {
+		newVulnerabilitiesOrViolations = append(
+			getUniqueVulnerabilityOrViolationRows(simpleJsonTarget.Vulnerabilities, simpleJsonSource.Vulnerabilities),
+			getUniqueVulnerabilityOrViolationRows(simpleJsonTarget.SecurityViolations, simpleJsonSource.SecurityViolations)...,
+		)
+	}
+
+	var newLicenses []formats.LicenseRow
+	if len(simpleJsonSource.LicensesViolations) > 0 {
+		newLicenses = getUniqueLicenseRows(simpleJsonTarget.LicensesViolations, simpleJsonSource.LicensesViolations)
+	}
+
+	var newIacs []formats.SourceCodeRow
+	if len(simpleJsonSource.Iacs) > 0 {
+		newIacs = createNewSourceCodeRows(simpleJsonTarget.Iacs, simpleJsonSource.Iacs)
+	}
+	var newSecrets []formats.SourceCodeRow
+	if len(simpleJsonSource.Secrets) > 0 {
+		newSecrets = createNewSourceCodeRows(simpleJsonTarget.Secrets, simpleJsonSource.Secrets)
+	}
+	var newSast []formats.SourceCodeRow
+	if len(simpleJsonSource.Sast) > 0 {
+		newSast = createNewSourceCodeRows(simpleJsonTarget.Sast, simpleJsonSource.Sast)
+	}
+
+	return &utils.IssuesCollection{
+		Vulnerabilities: newVulnerabilitiesOrViolations,
+		Iacs:            newIacs,
+		Secrets:         newSecrets,
+		Sast:            newSast,
+		Licenses:        newLicenses,
+	}, nil
+}
+
+func createNewSourceCodeRows(targetResults, sourceResults []formats.SourceCodeRow) []formats.SourceCodeRow {
+	targetSourceCodeVulnerabilitiesKeys := datastructures.MakeSet[string]()
+	for _, row := range targetResults {
+		if row.Fingerprint != "" {
+			targetSourceCodeVulnerabilitiesKeys.Add(row.Fingerprint)
+		} else {
+			targetSourceCodeVulnerabilitiesKeys.Add(row.File + row.Snippet)
+		}
+	}
+	var addedSourceCodeVulnerabilities []formats.SourceCodeRow
+	for _, row := range sourceResults {
+		if !targetSourceCodeVulnerabilitiesKeys.Exists(row.File+row.Snippet) && !targetSourceCodeVulnerabilitiesKeys.Exists(row.Fingerprint) {
+			addedSourceCodeVulnerabilities = append(addedSourceCodeVulnerabilities, row)
+		}
+	}
+	return addedSourceCodeVulnerabilities
+}
+
+func getUniqueVulnerabilityOrViolationRows(targetRows, sourceRows []formats.VulnerabilityOrViolationRow) []formats.VulnerabilityOrViolationRow {
+	existingRows := make(map[string]formats.VulnerabilityOrViolationRow)
+	var newRows []formats.VulnerabilityOrViolationRow
+	for _, row := range targetRows {
+		existingRows[utils.GetVulnerabiltiesUniqueID(row)] = row
+	}
+	for _, row := range sourceRows {
+		if _, exists := existingRows[utils.GetVulnerabiltiesUniqueID(row)]; !exists {
+			newRows = append(newRows, row)
+		}
+	}
+	return newRows
+}
+
+func getUniqueLicenseRows(targetRows, sourceRows []formats.LicenseRow) []formats.LicenseRow {
+	existingLicenses := make(map[string]formats.LicenseRow)
+	var newLicenses []formats.LicenseRow
+	for _, row := range targetRows {
+		existingLicenses[getUniqueLicenseKey(row)] = row
+	}
+	for _, row := range sourceRows {
+		if _, exists := existingLicenses[getUniqueLicenseKey(row)]; !exists {
+			newLicenses = append(newLicenses, row)
+		}
+	}
+	return newLicenses
+}
+
+func getUniqueLicenseKey(license formats.LicenseRow) string {
+	return license.LicenseKey + license.ImpactedDependencyName + license.ImpactedDependencyType
+}
+
+func aggregateScanResults(scanResults []services.ScanResponse) services.ScanResponse {
+	aggregateResults := services.ScanResponse{
+		Violations:      []services.Violation{},
+		Vulnerabilities: []services.Vulnerability{},
+		Licenses:        []services.License{},
+	}
+	for _, scanResult := range scanResults {
+		aggregateResults.Violations = append(aggregateResults.Violations, scanResult.Violations...)
+		aggregateResults.Vulnerabilities = append(aggregateResults.Vulnerabilities, scanResult.Vulnerabilities...)
+		aggregateResults.Licenses = append(aggregateResults.Licenses, scanResult.Licenses...)
+	}
+	return aggregateResults
+}
---
+++ frogbot-master/utils/testsutils.go
@@ -0,0 +1,218 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jfrog/jfrog-client-go/xsc/services"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	goGitConfig "github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	biutils "github.com/jfrog/build-info-go/utils"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/jfrog/jfrog-client-go/utils/io/fileutils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	ValidConfigProfile          = "default-profile"
+	InvalidPathConfigProfile    = "invalid-path-from-root-profile"
+	InvalidModulesConfigProfile = "invalid-modules-profile"
+)
+
+// Receive an environment variables key-values map, set and assert the environment variables.
+// Return a callback that sets the previous values.
+func SetEnvAndAssert(t *testing.T, env map[string]string) {
+	for key, val := range env {
+		setEnvAndAssert(t, key, val)
+	}
+}
+
+// Make sure the environment variables does not contain any Frogbot variables
+func AssertSanitizedEnv(t *testing.T) {
+	for _, env := range os.Environ() {
+		assert.False(t, strings.HasPrefix(env, "JF_"))
+	}
+}
+
+func setEnvAndAssert(t *testing.T, key, value string) {
+	assert.NoError(t, os.Setenv(key, value))
+}
+
+func unsetEnvAndAssert(t *testing.T, key string) {
+	assert.NoError(t, os.Unsetenv(key))
+}
+
+// This function takes a map of environment variables and sets them, and returns a callback to UNSET them all
+func SetEnvsAndAssertWithCallback(t *testing.T, envs map[string]string) func() {
+	for key, val := range envs {
+		setEnvAndAssert(t, key, val)
+	}
+	return func() {
+		for key := range envs {
+			unsetEnvAndAssert(t, key)
+		}
+	}
+}
+
+func MockHasConnection() *UrlAccessChecker {
+	return &UrlAccessChecker{url: "url", connected: true}
+}
+
+// Create a temporary directory and copy the content of "testdata/testDir" into it
+func CopyTestdataProjectsToTemp(t *testing.T, testDir string) (tmpDir string, restoreFunc func()) {
+	// Copy project to a temporary directory
+	tmpDir, err := fileutils.CreateTempDir()
+	assert.NoError(t, err)
+	err = biutils.CopyDir(filepath.Join("..", "testdata", testDir), tmpDir, true, []string{})
+	assert.NoError(t, err)
+	restoreFunc = func() {
+		assert.NoError(t, fileutils.RemoveTempDir(tmpDir))
+	}
+	return
+}
+
+func ChangeToTempDirWithCallback(t *testing.T) (string, func() error) {
+	tmpDir, err := fileutils.CreateTempDir()
+	assert.NoError(t, err)
+	callback, err := Chdir(tmpDir)
+	assert.NoError(t, err)
+	return tmpDir, callback
+}
+
+// Check connection details with JFrog instance.
+// Return a callback method that restores the credentials after the test is done.
+func VerifyEnv(t *testing.T) (server config.ServerDetails, restoreFunc func()) {
+	url := strings.TrimSuffix(os.Getenv(JFrogUrlEnv), "/")
+	username := os.Getenv(JFrogUserEnv)
+	password := os.Getenv(JFrogPasswordEnv)
+	token := os.Getenv(JFrogTokenEnv)
+	if url == "" {
+		assert.FailNow(t, fmt.Sprintf("'%s' is not set", JFrogUrlEnv))
+	}
+	if token == "" && (username == "" || password == "") {
+		assert.FailNow(t, fmt.Sprintf("'%s' or '%s' and '%s' are not set", JFrogTokenEnv, JFrogUserEnv, JFrogPasswordEnv))
+	}
+	server.Url = url
+	server.XrayUrl = url + "/xray/"
+	server.ArtifactoryUrl = url + "/artifactory/"
+	server.User = username
+	server.Password = password
+	server.AccessToken = token
+	restoreFunc = func() {
+		SetEnvAndAssert(t, map[string]string{
+			JFrogUrlEnv:          url,
+			JFrogTokenEnv:        token,
+			JFrogUserEnv:         username,
+			JFrogPasswordEnv:     password,
+			GitAggregateFixesEnv: "FALSE",
+		})
+	}
+	return
+}
+
+func CreateDotGitWithCommit(t *testing.T, wd, port string, repositoriesPath ...string) {
+	for _, repositoryPath := range repositoriesPath {
+		fullWdPath := filepath.Join(wd, repositoryPath)
+		dotGit, err := git.PlainInit(fullWdPath, false)
+		assert.NoError(t, err)
+		_, err = dotGit.CreateRemote(&goGitConfig.RemoteConfig{
+			Name: "origin",
+			URLs: []string{fmt.Sprintf("http://127.0.0.1:%s/%s", port, repositoryPath)},
+		})
+		assert.NoError(t, err)
+		worktree, err := dotGit.Worktree()
+		assert.NoError(t, err)
+		assert.NoError(t, worktree.AddWithOptions(&git.AddOptions{All: true}))
+		_, err = worktree.Commit("first commit", &git.CommitOptions{
+			Author: &object.Signature{
+				Name:  "JFrog-Frogbot",
+				Email: "eco-system+frogbot@jfrog.com",
+				When:  time.Now(),
+			},
+		})
+		assert.NoError(t, err)
+	}
+}
+
+func CreateTempJfrogHomeWithCallback(t *testing.T) (string, func()) {
+	newJfrogHomeDir, err := fileutils.CreateTempDir()
+	assert.NoError(t, err)
+	prevJfrogHomeDir := os.Getenv(JfrogHomeDirEnv)
+	assert.NoError(t, os.Setenv(JfrogHomeDirEnv, newJfrogHomeDir))
+	return newJfrogHomeDir, func() {
+		assert.NoError(t, os.Setenv(JfrogHomeDirEnv, prevJfrogHomeDir))
+		assert.NoError(t, fileutils.RemoveTempDir(newJfrogHomeDir))
+	}
+}
+
+func CreateXscMockServerForConfigProfile(t *testing.T) (mockServer *httptest.Server, serverDetails *config.ServerDetails) {
+	mockServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		secondModule := services.Module{
+			ModuleId:     999,
+			ModuleName:   "second-module",
+			PathFromRoot: ".",
+			ScanConfig: services.ScanConfig{
+				ScanTimeout:                  0,
+				ExcludePattern:               "",
+				EnableScaScan:                false,
+				EnableContextualAnalysisScan: false,
+			},
+		}
+
+		switch {
+		case strings.HasPrefix(r.RequestURI, "/xsc/api/v1/profile/"):
+			assert.Equal(t, http.MethodGet, r.Method)
+			if r.RequestURI == "/xsc/api/v1/profile/"+ValidConfigProfile {
+				w.WriteHeader(http.StatusOK)
+			} else {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+
+			content, err := os.ReadFile("../testdata/configprofile/configProfileExample.json")
+			assert.NoError(t, err)
+
+			if r.RequestURI == "/xsc/api/v1/profile/"+InvalidModulesConfigProfile {
+				// Adding a second module to make the profile invalid, as we currently support ONLY profile with a single module
+				var profile services.ConfigProfile
+				err = json.Unmarshal(content, &profile)
+				assert.NoError(t, err)
+				profile.Modules = append(profile.Modules, secondModule)
+				content, err = json.Marshal(profile)
+				assert.NoError(t, err)
+			}
+
+			if r.RequestURI == "/xsc/api/v1/profile/"+InvalidPathConfigProfile {
+				// Changing 'path_from_root' to a path different from '.' to make the module invalid, as we currently support ONLY a single module with '.' path
+				updatedContent := string(content)
+				updatedContent = strings.Replace(updatedContent, `"path_from_root": "."`, `"path_from_root": "backend"`, 1)
+				content = []byte(updatedContent)
+			}
+
+			_, err = w.Write(content)
+			assert.NoError(t, err)
+
+		case r.RequestURI == "/xsc/api/v1/system/version":
+			_, err := w.Write([]byte(fmt.Sprintf(`{"xsc_version": "%s"}`, services.ConfigProfileMinXscVersion)))
+			assert.NoError(t, err)
+		default:
+			assert.Fail(t, "received an unexpected request")
+		}
+	}))
+
+	url := mockServer.URL
+	serverDetails = &config.ServerDetails{
+		Url:     url + "/",
+		XrayUrl: url + "/xray/",
+		XscUrl:  url + "/xsc/",
+	}
+	return
+}
